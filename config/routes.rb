d92bcc3feb32c56d9fd58f778eb0773939b5fe46
bhargavi <bhagi.sb@gmail.com>
2023-03-13 12:40:08 +0530

    change commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get '/' => 'pages#home'
+  get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact-us'
+  get 'team' => 'pages#team'
+  get 'testimonials' => 'pages#testimonials'
+  get 'services' => 'pages#services'
+  get 'portfolio' => 'pages#portfolio'
+  get 'pricing' => 'pages#pricing'
+  get 'blog' => 'pages#blog'
+  get 'terms' => 'pages#terms'
+  get 'privacy' => 'pages#privacy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
